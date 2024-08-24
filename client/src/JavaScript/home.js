afb0ed7e9411a92b60bb5f9e078925560ddb8798
写代码ing <111549045+CMatrixHacker@users.noreply.github.com>
2024-08-24 21:05:47 +0800

    update: 添加了一个关于不同主题的更新

--- client/src/JavaScript/home.js
+++ client/src/JavaScript/home.js
@@ -1,5 +1,23 @@
-if (!("WebSocket" in window)){
-    alert("您的浏览器不支持websocket!我们无法和服务器建立连接!");
+function browserInfo(){
+    var userAgent = navigator.userAgent;
+    var isOpera = userAgent.indexOf("Opera") > -1;
+    if (isOpera) {      //Opera
+        return "Opera"
+    }
+    if (userAgent.indexOf("Firefox") > -1) {     //Firefox
+        return "FF";
+    }
+    if (userAgent.indexOf("Chrome") > -1){   //Chrome
+        return "Chrome";
+    }
+    if (userAgent.indexOf("Safari") > -1) {     //Safari
+        return "Safari";
+    }
+    if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+        return "IE";
+    }        //IE
 }
 
-docm
+if(browserInfo() == "Safari"){
+    alert("我们建议您使用Chrome浏览器访问本网站,因为本网站中所有的Css都是基于Firefox编写的,在Safari中可能会出现一些问题,给您带来不便,我们深感抱歉")
+}
---
+++ server/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+node main.js
+# node server.js & npm run client
+# node server.js & npm run client & npm run client2
+# node server.js & npm run client & npm run client2 & npm run client3
+# node server.js & npm run client & npm run client2 & npm run client3 & npm run client4
